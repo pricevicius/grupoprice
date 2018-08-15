bb9689eda319eac98562ea3623c5a82d664e0582
Leonardo Pricevicius <leonardo@okn.com.br>
2018-08-15 12:03:55 -0300

    remove folders

--- build/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.resolve(`${__dirname}./../config/config.json`))[env];
-const operatorsAliases = { $in: Sequelize.Op.in };
-exports.default = Object.assign({ operatorsAliases }, config);
